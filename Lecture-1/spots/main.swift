333535003b17d467dc0257a3b284efa48c9ee1b1
Kiril Krutiajev <nonamekk@tutanota.com>
2022-09-20 23:53:56 +0300

    folders

---
+++ Lecture-1/spots/main.swift
@@ -0,0 +1,14 @@
+//import Foundation
+
+var animal: UInt8 = 32
+var baam: UInt8 = 0;
+var bwt: UInt8 = 0;
+
+for i in 0...animal {
+    if ((UInt8.random(in: 0..<animal)) > i) {
+        bwt+=1
+    } else {baam+=1}
+}
+
+print("Black spots", baam)
+print("White spots", bwt)
